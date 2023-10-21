d3bf613a6e72c15c9cd912c644e4ae169da53600
furicle <binlinux@hotmail.com>
2023-10-21 09:50:49 -0400

    Whitespace and formatting changes
    Moved the write-verbose blocks to @" strings

--- ConvertFrom-CalibreToLibraryThing.ps1
+++ ConvertFrom-CalibreToLibraryThing.ps1
@@ -1,40 +1,41 @@
 <#
 .SYNOPSIS
-	Help get book data from Calibre to Library Thing
+    Help get book data from Calibre to Library Thing
 .DESCRIPTION
-	Exports JSON from the calibre database and converts it to csv files useful with Library Thing
+    Exports JSON from the calibre database and converts it to csv files useful with Library Thing
 .PARAMETER FileNamePrefix
-	If specified, will use the as the provided prefix in the output csv file names
+    If specified, will use the as the provided prefix in the output csv file names
 .PARAMETER FilePath
-	If specified, will output csv files into that directory
+    If specified, will output csv files into that directory
 .EXAMPLE
-	PS> ./ConvertFrom-CalibreToLibraryThing.ps1
-	Launches calibredb and exports all entries into two csv files, saved in your Downloads folder, then opens the Library Thing website.
+    PS> ./ConvertFrom-CalibreToLibraryThing.ps1
+    Launches calibredb and exports all entries into two csv files, saved in your Downloads folder, then opens the Library Thing website.
 .EXAMPLE
-	PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --SkipBrowser
-	Launches calibredb and exports all entries into two csv files, saved in your Downloads folder, but does not open the Library Thing website.
+    PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --SkipBrowser
+    Launches calibredb and exports all entries into two csv files, saved in your Downloads folder, but does not open the Library Thing website.
 .EXAMPLE
-	PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --SingleFile
-	Launches calibredb and exports all entries into one csv file, saved in your Downloads folder
+    PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --SingleFile
+    Launches calibredb and exports all entries into one csv file, saved in your Downloads folder
 .EXAMPLE
-	PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --FilePath ./Out
-	Launches calibredb and exports all entries into two csv files, saved in a folder called Out in the same directory as this conversion script
+    PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --FilePath ./Out
+    Launches calibredb and exports all entries into two csv files, saved in a folder called Out in the same directory as this conversion script
 .EXAMPLE
-	PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --FileName CollectionA
-	Launches calibredb and exports all entries into two csv files, named CollectionA-NoISBN-{currentdatetime}.csv and CollectionA-ISBNOnly-{currentdatetime}.csv
+    PS> ./ConvertFrom-CalibreToLibraryThing.ps1 --FileName CollectionA
+    Launches calibredb and exports all entries into two csv files, named CollectionA-NoISBN-{currentdatetime}.csv and CollectionA-ISBNOnly-{currentdatetime}.csv
 .LINK
-	https://github.com/furicle/ConvertFrom-CalibreToLibraryThing
+    https://github.com/furicle/ConvertFrom-CalibreToLibraryThing
 .NOTES
-	Author: furicle | License: GPL-3
+    Author: furicle | License: GPL-3
 #>
+
 
 # set up required variables
 [CmdletBinding()]
 param(
-	[string]$FileNamePrefix  = "Import To LibraryThing",
-	[string]$FilePath        = "~/Downloads/",
-	[switch]$SkipBrowser
-	)
+        [string]$FileNamePrefix  = "Import To LibraryThing",
+        [string]$FilePath        = "~/Downloads/",
+        [switch]$SkipBrowser
+)
 
 # These variables aren't likely to be changed as an option
 [string]$dateStamp             = Get-Date -Format yyyy-MM-dd_HH-mm-ss
@@ -43,18 +44,29 @@ param(
 [string]$calibreDBHelpURL      = "https://manual.calibre-ebook.com/generated/en/calibredb.html"
 
 
-
 # check for calibredb
 try {
-    " Checking for Calibre..."
-    calibredb.exe --version
+    Write-Verbose " Checking for Calibre..."
+        calibredb.exe --version
 }
 catch {
-    " Calibre command line database interface not found"
-    " Typing calibredb.exe --version in a PowerShell window has to work for this script to function"
-    " Please confirm Calibre installed and the calibredb command on your PATH"
-    " See $calibreDBHelpURL for more info on the command"
-    exit 1
+@" 
+
+        Typing
+
+        calibredb.exe --version
+
+        in a PowerShell window has to work for this script to function
+
+        Please confirm
+        * Calibre is installed and
+        * the calibredb command is on your `$PATH
+
+        See $calibreDBHelpURL for more info.
+
+"@
+        Write-Error  "Calibre command line database interface not found"
+        exit 1
 }
 
 # export library to json
@@ -73,42 +85,61 @@ $csvData = $trimmedData | ConvertTo-Csv -NoTypeInformation
 $csvData | Out-File -Encoding utf8 -FilePath $OutPath/$csvFileName
 $count = $csvData.Count
 
-Write-Verbose " "
-Write-Verbose " Saved CSV file -> $csvFileName "
-Write-Verbose " in folder      -> $OutPath "
-Write-Verbose " "
-Write-Verbose " It has $count books in it "
-Write-Verbose " "
-Write-Verbose " You may wish to review the whole file for sanity "
-Write-Verbose " before you upload to Library Thing "
-Write-Verbose " "
+Write-Verbose @" 
+
+     Saved CSV file -> $csvFileName
+     in folder      -> $OutPath
+
+     It has $count books in it
+
+     You may wish to review the whole file for sanity
+     before you upload to Library Thing
+
+"@
 
 # open the csv with default editor for further review
-Write-Verbose " Opening csv file editor "
-Write-Verbose " REMEMBER ISBN numbers often start with a zero, so you "
-Write-Verbose " MUST import that column as text, not a number, "
-Write-Verbose " or your ISBN numbers will be mangled. "
+Write-Verbose @" 
+
+     Opening csv file editor...
+
+     REMEMBER ISBN numbers often start with a zero, so you
+     MUST import that column as text, not a number,
+     or your ISBN numbers will be mangled.
+
+ "@
 
 Invoke-Item $OutPath/$csvFileName
 
 # open the Library Thing Import Page
-Write-Verbose " Opening Library Thing Import Page "
-Write-Verbose " This may not work if you aren't already logged into Library Thing "
-Write-Verbose " You can just go $LibraryThingImportURL manually after logging in "
+Write-Verbose @" 
+
+    Opening Library Thing Import Page...
+
+    This may not work if you aren't already logged into Library Thing.
+    You can just go $LibraryThingImportURL manually after logging in.
+
+"@
 
 if ( ! $SkipBrowser ) {
-	Start-Process $LibraryThingImportURL
+    Start-Process $LibraryThingImportURL
 } else {
-	Write-Verbose " Not opening Library Thing Import Page "
-	Write-Verbose " Visit $LibraryThingImportURL after logging in "
-	Write-Verbose " to upload your results "
+    Write-Verbose @" 
+
+    NOT opening Library Thing Import Page.
+
+    Visit $LibraryThingImportURL after logging in to upload your results.
+
+"@
+
 }
 
-Write-Verbose ""
-Write-Verbose " -------------"
-Write-Verbose " - All done! - "
-Write-Verbose " -------------"
-Write-Verbose ""
+Write-Verbose @" 
+
+     -------------
+     - All done! -
+     -------------
+
+"@
 
 
 # vim: set filetype=ps1 syntax=ps1 ts=4 sw=4 tw=0 :
